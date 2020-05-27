c7ee10d8f621d4d8baa379908917439140f8c9f3
spv <64748227+spacepilotAV@users.noreply.github.com>
2020-05-27 12:27:22 -0400

    Update librfinder.js

--- librfinder.js
+++ librfinder.js
@@ -6,17 +6,17 @@
 function guessOUM(width, height) {
 	// only works in portrait right now
 	if (width == 640 && height == 1136) {
-    return [16777216, 268435456];
+		return [16777216, 268435456];
 	}
 	else if (width == 750 && height == 1334) {
-    return [33554432, 268435456];
+		return [33554432, 268435456];
 	}
 	else if (width > 750 && height > 1334) {
-    if (navigator.userAgent.includes("iPad")) {
+		if (navigator.userAgent.includes("iPad")) {
 			return [67108864, 268435456];
 		}
 		else {
-      return [33554432, 268435456];
+			return [33554432, 268435456];
 		}
 	}
 }
